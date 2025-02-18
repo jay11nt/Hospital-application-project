1641086ced76ce0befed480053628251c0c94200
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-18 22:23:07 +0530

    Update Doctor.java

--- main/entity/Doctor.java
+++ main/entity/Doctor.java
@@ -31,7 +31,8 @@ public class Doctor
         return id;
     }
     
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }    
     public void setName(String name) {
