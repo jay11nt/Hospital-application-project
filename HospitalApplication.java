950fe94d9212a73bfdb366c7722db75e122e189f
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-02-20 20:23:05 +0530

    Update HospitalApplication.java

--- HospitalApplication.java
+++ HospitalApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HospitalApplication {
-  public static void main(String[] args) {
+public class HospitalApplication 
+{
+  public static void main(String[] args) 
+  {
     SpringApplication.run(HospitalApplication.class, args);
   }
 }
